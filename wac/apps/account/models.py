d36fc514a555f862eb967995d5a372bae20755bb
Mark Gituma <gitumarkk@gmail.com>
2018-01-27 01:18:00 -0500

    added user and core module

--- wac/apps/account/models.py
+++ wac/apps/account/models.py
@@ -2,22 +2,86 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# class Profile(models.Model):
-#     """
-#     Extends from the user model
-#     """
-#     user = models.OneToOneField(
-#         User
-#     )
-#     created_at = models.DateTimeField(
-#         auto_now_add=True
-#     )
+class Profile(models.Model):
+    """
+    Extends from the user model
+    """
+    HE = 'HE'
+    SHE = 'SHE'
+    THEY = 'THEY'
+    PRONOUNS_CHOICE = (
+        (HE, HE),
+        (SHE, SHE),
+        (THEY, THEY)
+    )
 
-#     def display_name(self):
-#         return u"%s %s".strip() % (self.user.first_name, self.user.last_name)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
 
-#     def __str__(self):
-#         return self.display_name()
+    woman = models.NullBooleanField(
+        null=True,
+        blank=True
+    )
 
-#     def __unicode__(self):
-#         return self.display_name()
+    poc = models.NullBooleanField(
+        null=True,
+        blank=True
+    )
+
+    pronouns = models.CharField(
+        max_length=10,
+        choices=PRONOUNS_CHOICE,
+        null=True,
+        blank=True
+    )
+
+    position = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+
+    organization = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+
+    description = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+
+    twitter = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+
+    linkedin = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+
+    website = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def display_name(self):
+        return u"%s %s".strip() % (self.user.first_name, self.user.last_name)
+
+    def __str__(self):
+        return self.display_name()
+
+    def __unicode__(self):
+        return self.display_name()
--- wac/settings.py
+++ wac/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # App
+    'wac.apps.core',
     'wac.apps.account'
 ]
 
