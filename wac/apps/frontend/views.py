74ac803efda8f462b030a135421ea28d323c938c
Mark Gituma <gitumarkk@gmail.com>
2018-01-31 14:24:27 -0500

    render authenticated view

--- wac/apps/frontend/views.py
+++ wac/apps/frontend/views.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
+    context = {}
+    return render(request, 'frontend/home.html', context)
+
+
+@login_required
+def profile(request):
     context = {}
     return render(request, 'frontend/home.html', context)
