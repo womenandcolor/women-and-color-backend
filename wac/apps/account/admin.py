e60bd7e83f83b8092125f804ea85e8f7e4440351
Mark Gituma <gitumarkk@gmail.com>
2018-01-31 14:32:38 -0500

    added admin

--- wac/apps/account/admin.py
+++ wac/apps/account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+# App
+from wac.apps.account.models import (
+    Profile
+)
+
+
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'created_at'
+    ]
+
