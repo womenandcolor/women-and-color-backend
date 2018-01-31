fbf28f0ddd9956e8e94922867f9c63aef3abc4b7
Mark Gituma <gitumarkk@gmail.com>
2018-01-31 14:24:54 -0500

    added profile viewset

--- wac/apps/account/api/viewsets.py
+++ wac/apps/account/api/viewsets.py
@@ -2,7 +2,11 @@
 from django.contrib.auth.models import User
 
 # App
-from wac.apps.account.api.serializers import UserSerializer
+from wac.apps.account.api.serializers import (
+    UserSerializer,
+    ProfileSerializer
+)
+from wac.apps.account.models import Profile
 
 # Rest Framework
 from rest_framework import viewsets
@@ -11,3 +15,8 @@ from rest_framework import viewsets
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
