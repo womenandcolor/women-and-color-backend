7a991fa23590856c8becdcda200ce1598bc29e12
Mark Gituma <gitumarkk@gmail.com>
2018-01-31 14:25:03 -0500

    added profile routeer

--- wac/apps/account/api/routers.py
+++ wac/apps/account/api/routers.py
@@ -1,9 +1,10 @@
 # App
-from wac.apps.account.api.viewsets import UserViewSet
+from wac.apps.account.api.viewsets import UserViewSet, ProfileViewSet
 
 # Rest Framework
 from rest_framework import routers
 
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'users', UserViewSet)
+router.register(r'profiles', ProfileViewSet)
