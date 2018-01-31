09c6f1c942dc4aca899d6402c5f35cb28d51c7da
Mark Gituma <gitumarkk@gmail.com>
2018-01-31 14:24:31 -0500

    updated urls

--- wac/urls.py
+++ wac/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('wac.apps.frontend.urls', namespace='frontend')),
-    path('api/v1/', include('wac.apps.account.api.urls', namespace='account-api')),
+    path(r'api/v1/', include('wac.apps.core.api.urls', namespace='core-api')),
+    path(r'api/v1/', include('wac.apps.account.api.urls', namespace='account-api')),
 ]
