19cf9e9ade930187ef9b29c0fb2e6f78c9a5e646
Sharon <sharon.peishan.kennedy@gmail.com>
2018-02-20 13:04:51 -0500

    remove STATICFILES_DIRS in settings.py

--- wac/settings.py
+++ wac/settings.py
@@ -147,9 +147,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
