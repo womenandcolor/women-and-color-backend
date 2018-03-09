f56b42d3ad9043f16b29d65a80dc6812b6e1dc57
Sharon <sharon.peishan.kennedy@gmail.com>
2018-03-09 15:30:29 -0500

    search query on topics list

--- wac/apps/core/api/viewsets.py
+++ wac/apps/core/api/viewsets.py
@@ -4,12 +4,24 @@ from wac.apps.core.models import Location, Topic
 
 # Rest Framework
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
 
+
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+
+    def list(self, request):
+        queryset = Topic.objects.all()
+
+        if 'q' in request.GET:
+            q = request.GET.get('q')
+            queryset = queryset.filter(topic__icontains=q)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
