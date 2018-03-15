8a735dc2060ebebdf9fdd5b67374e4638506acbd
Sharon <sharon.peishan.kennedy@gmail.com>
2018-03-15 14:35:05 -0400

    serve static files on production

--- wac/settings.py
+++ wac/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'wac.urls'
@@ -153,6 +154,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # ====================== REST FRAMEWORK ====================== #
