99bd6dafc87adadebab8871b2d40195ec682ee27
Sharon <sharon.peishan.kennedy@gmail.com>
2018-04-22 14:47:18 -0400

    update default permissions

--- wac/settings.py
+++ wac/settings.py
@@ -159,6 +159,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
 }
 # ============================================================ #
