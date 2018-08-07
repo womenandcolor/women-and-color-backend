eeaff84cd925c7aba65054096d6a81866149317a
Sharon <sharon.peishan.kennedy@gmail.com>
2018-08-06 23:19:58 -0400

    add speaker contact form to admin view

--- wac/apps/contact_speaker/admin.py
+++ wac/apps/contact_speaker/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+# App
+from wac.apps.contact_speaker.models import (
+    ContactForm
+)
+
 # Register your models here.
+@admin.register(ContactForm)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = [
+        'full_name',
+        'email',
+        'event_date',
+        'event_name',
+        'profile',
+        'comments',
+        'created_at',
+    ]
+
