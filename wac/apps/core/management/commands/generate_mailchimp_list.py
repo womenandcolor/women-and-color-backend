6c8e95dce5da8c276c569a64a266c7107d3a6c49
Sharon <sharon.peishan.kennedy@gmail.com>
2018-08-30 10:07:46 -0400

    log results of mailchimp subscription command

--- wac/apps/core/management/commands/generate_mailchimp_list.py
+++ wac/apps/core/management/commands/generate_mailchimp_list.py
@@ -7,11 +7,13 @@ from django.http import HttpRequest
 from django.middleware.csrf import get_token
 from allauth.account.views import PasswordResetView
 from mailchimp3 import MailChimp
+from mailchimp3.mailchimpclient import MailChimpError
 
 # App
 from wac.apps.accounts.models import Profile
 
 import hashlib
+
 
 class Command(BaseCommand):
     help = "Add all users who are not already subscribed to a mailing list. Takes two arguments: 'target_list_id' is the id of the list to subscribe the email to and 'check_list_id' is the id of the list to check if the email is already subscribed."
@@ -24,29 +26,35 @@ class Command(BaseCommand):
             subscriber_hash=email_hash
         )
 
-        print("subscriber")
-        print(subscriber)
-
-        return subscriber.status == 'subscribed'
+        self.stdout.write(self.style.SUCCESS('Email already subscribed to {}: {}'.format(list_id, profile.user.email)))
 
 
     def subscribe_to_list(self, profile, list_id, client):
         email_hash = hashlib.md5(profile.user.email.encode('utf-8')).hexdigest()
 
-        client.lists.members.create_or_update(
-            list_id=list_id,
-            subscriber_hash=email_hash,
-            data={
-                'email_address': profile.user.email,
-                'status': 'subscribed',
-                'status_if_new': 'subscribed',
-                'merge_fields': {
-                  'FNAME': profile.first_name,
-                  'LNAME': profile.last_name,
-                  'ICITY': profile.location.city if profile.location else "undisclosed location",
-            },
-          }
-        )
+        data = {
+            'email_address': profile.user.email,
+            'status': 'subscribed',
+            'status_if_new': 'subscribed',
+            'merge_fields': {
+              'FNAME': profile.first_name,
+              'LNAME': profile.last_name,
+              'IWOMAN': "Yes" if profile.woman else "No",
+              'IPOC': "Yes" if profile.poc else "No",
+              'ICITY': profile.location.city if profile.location else "undisclosed location",
+            }
+        }
+
+        try:
+            client.lists.members.create_or_update(
+                list_id=list_id,
+                subscriber_hash=email_hash,
+                data=data
+            )
+            self.stdout.write(self.style.SUCCESS('Successfully subscribed email {} to list {}'.format(profile.user.email, list_id)))
+        except MailChimpError as e:
+            self.stdout.write(self.style.WARNING('Unable to add subscriber with email {}'.format(profile.user.email)))
+            self.stdout.write(self.style.WARNING('Invalid data: {}'.format(data)))
 
     def add_arguments(self, parser):
         parser.add_argument('target_list_id')
@@ -59,12 +67,10 @@ class Command(BaseCommand):
         client = MailChimp(mc_api=settings.MAILCHIMP_API_KEY, timeout=10.0)
 
         for profile in profiles:
-            already_subscribed = self.check_if_subscribed(profile, check_list_id, client)
-            if already_subscribed:
-                self.stdout.write(self.style.SUCCESS('Email already subscribed to {}: {}'.format(check_list_id, profile.user.email)))
-            else:
+            try:
+                self.check_if_subscribed(profile, check_list_id, client)
+            except MailChimpError as e:
                 self.subscribe_to_list(profile, target_list_id, client)
-                self.stdout.write(self.style.SUCCESS('New email subscribed to {}: {}'.format(target_list_id, profile.user.email)))
 
 
 
